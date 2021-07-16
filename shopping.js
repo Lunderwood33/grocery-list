8a6e737227432152f5a991a593ecba5cb4028246
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-07-16 13:21:19 -0500

    first commit

---
+++ shopping.js
@@ -0,0 +1,61 @@
+
+
+const groceryList = [
+    
+    {
+        id: 1,
+        item: "Milk",
+        type: "Dairy",
+        price: 3.08,
+        quantity: 1
+    },
+    
+    {
+        id: 2,
+        item: "Eggs",
+        type: "Poultry",
+        price: 2.98,
+        quantity: 1
+    },
+
+    {
+        id: 3,
+        item: "Pizza rolls",
+        type: "Fats",
+        price: 7.25,
+        quantity: 3
+    },
+
+    {
+        id: 4,
+        item: "Strawberries",
+        type: "Fruit",
+        price: 1.50,
+        quantity: 1
+    },
+
+    {
+        id: 5,
+        item: "Sandwich Buns",
+        type: "Starch",
+        price: 3.75,
+        quantity: 1
+    },
+
+    {
+        id: 6,
+        item: "Hot Dogs",
+        type: "Protein",
+        price: 8.27,
+        quantity: 2
+    },
+
+    {
+        id: 7,
+        item: "Ketchup",
+        type: "Condiment",
+        price: 1.97,
+        quantity: 1
+    }
+]
+console.log(groceryList)
